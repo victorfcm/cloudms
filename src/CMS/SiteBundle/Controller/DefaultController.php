2ccef6c2387d891d57baa79408f21d0a898071de
Victor Freitas <contato@vfreitas.com>
2013-04-10 19:06:58 -0300

    that's all folks

--- src/CMS/SiteBundle/Controller/DefaultController.php
+++ src/CMS/SiteBundle/Controller/DefaultController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/", name="front_index")
      * @Template()
@@ -17,16 +18,16 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $this->setMenu();
-        
+
         return array('links' => array(
-            'Admin' => $this->generateUrl('post_cindex'),
-            'Master' => $this->generateUrl('post')
+                'Admin' => $this->generateUrl('post_cindex'),
+                'Master' => $this->generateUrl('post')
             ),
             'pages' => $this->pages,
             'postTypes' => $this->postTypes
-            );
+        );
     }
-    
+
     /**
      * @Route("/page/{id}", name="front_page")
      * @Template()
@@ -34,17 +35,27 @@ class DefaultController extends Controller
     public function pageAction($id)
     {
         $this->setMenu();
-        
+
         $em = $this->getDoctrine()->getManager();
-        
+
         $page = $em->getRepository("CMSStoreBundle:Post")->find($id);
-        
-        return array('page' => $page,
+
+        $data = array('page' => $page,
             'pages' => $this->pages,
             'postTypes' => $this->postTypes
-            );
+        );
+
+        if ($this->getTemplate($page->getPostType()->getName(), 'page'))
+        {
+            $name = strtolower($page->getPostType()->getName());
+            $_r = $this->renderView("CMSSiteBundle:Default:page.".$name.".html.twig", $data);
+
+            return new Response($_r);
+        }
+
+        return $data;
     }
-    
+
     /**
      * @Route("/posttype/{id}", name="front_post_type")
      * @Template()
@@ -52,28 +63,37 @@ class DefaultController extends Controller
     public function postTypeAction($id)
     {
         $this->setMenu();
-        
+
         $em = $this->getDoctrine()->getManager();
-        
+
         $postType = $em->getRepository("CMSStoreBundle:PostType")->find($id);
-        
-        return array('postType' => $postType,
+
+        $data = array('postType' => $postType,
             'pages' => $this->pages,
             'postTypes' => $this->postTypes
-            );
+        );
+
+        if ($this->getTemplate($postType->getName(), 'postType'))
+        {
+            $name = strtolower($postType->getName());
+            $_r = $this->renderView("CMSSiteBundle:Default:postType.".$name.".html.twig", $data);
+
+            return new Response($_r);
+        }
+
+        return $data;
     }
-    
+
     private function setMenu()
     {
         $this->getPages();
         $this->getPostTypes();
     }
-    
-    
+
     private function getPages()
     {
         $em = $this->getDoctrine()->getManager();
-        
+
         $pageType = $em->getRepository('CMSStoreBundle:PostType')->findOneByName('page');
         $pages = $em->getRepository('CMSStoreBundle:Post')
             ->createQueryBuilder('p')
@@ -82,14 +102,14 @@ class DefaultController extends Controller
             ->setParameter('pt', $pageType->getId())
             ->getQuery()
             ->execute();
-        
+
         $this->pages = $pages;
     }
-    
+
     private function getPostTypes()
     {
         $em = $this->getDoctrine()->getManager();
-        
+
         $pageTypes = $em->getRepository('CMSStoreBundle:PostType')
             ->createQueryBuilder('p')
             ->where('p.inMenu = true')
@@ -97,17 +117,17 @@ class DefaultController extends Controller
             ->setParameter('page', 'page')
             ->getQuery()
             ->execute();
-        
+
         $this->postTypes = $pageTypes;
     }
-    
+
     public function getTemplate($postType, $templateType = 'new')
     {
-        $filename = $templateType . '.' . $postType;
+        $filename = $templateType . '.' . strtolower($postType);
         $root = $this->get('kernel')->getRootDir();
-
+        
         $fs = new Filesystem();
-        return $fs->exists("$root/../src/CMS/SiteBundle/Resources/views/Post/$filename.html.twig");
+        return $fs->exists("$root/../src/CMS/SiteBundle/Resources/views/Default/$filename.html.twig");
     }
-}
 
+}
